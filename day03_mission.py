b88d62937ca7faa2628c8ce6e9ce01c678ffe1de
KimTaeYeong <12170548@inha.edu>
2023-01-16 17:49:43 +0900

    day03mission_6.2

--- day03_mission.py
+++ day03_mission.py
@@ -1,18 +1,13 @@
-A='duck'
-B='gourd'
-C='spitz'
+guess_me = 7
+number = 1
 
-print("%sy Mc%sface"%(A.capitalize(),A.capitalize()))
-print("%sy Mc%sface"%(B.capitalize(),B.capitalize()))
-print("%sy Mc%sface"%(C.capitalize(),C.capitalize()))
-print('\n')
-
-print('{}y Mc{}face'.format(A.capitalize(),A.capitalize()))
-print('{}y Mc{}face'.format(B.capitalize(),B.capitalize()))
-print('{}y Mc{}face'.format(C.capitalize(),C.capitalize()))
-print('\n')
-
-print(f'{A.capitalize()}y Mc{A.capitalize()}face')
-print(f'{B.capitalize()}y Mc{B.capitalize()}face')
-print(f'{C.capitalize()}y Mc{C.capitalize()}face')
-print('\n')
+while True :
+    if number < guess_me :
+        print('too low')
+    elif number==guess_me :
+        print('found it!')
+        break
+    else :
+        print('oops')
+        break
+    number+=1
